ebd3b8876af5476c0d672de206cb58492bebe26f
jj-am <smucka.smiley@gmail.com>
2020-04-18 12:47:13 +0200

    dodan timezone

--- datum.php
+++ datum.php
@@ -1,10 +1,12 @@
 <!-- UTF-8 trenutno nima supporta na Windowsih, zato se namesto tega vpiše "three letter string code". Za slovenščino tega string code-a nisem našel, zato je spodaj zapisan način, ki je napisan tudi na php.net -->
 <?php
   setlocale(LC_TIME, 'sl_SI.UTF-8');
-  echo strftime("%A, %e. %B. %Y");
+  echo 'Danes je: ' . strftime("%A, %e. %B. %Y") . "<br>";
+  echo 'Časovna razlika: ' . strftime("%z");
 ?>
 
 <!-- Spodaj pa uporabim še "date" -->
+<!-- Razlika med date in strftime je ta, da "date" ne moremo lokalizirat, medtem, ko strftime lahko lokaliziramo v poljuben jezik. -->
 Leto:
 <?php echo date ('Y'); ?>
 <br>
