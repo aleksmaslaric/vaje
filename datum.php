fa505b0200cf3af6054a1a94aeb65132b9ac595f
jj-am <smucka.smiley@gmail.com>
2020-04-18 10:14:12 +0200

    Dodani setlocale in strftime liniji.

--- datum.php
+++ datum.php
@@ -1,3 +1,10 @@
+<!-- UTF-8 trenutno nima supporta na Windowsih, zato se namesto tega vpiše "three letter string code". Za slovenščino tega string code-a nisem našel, zato je spodaj zapisan način, ki je napisan tudi na php.net -->
+<?php
+  setlocale(LC_ALL, 'sl_SI.UTF-8');
+  echo strftime("%A, %e. %B. %Y");
+?>
+
+<!-- Spodaj pa uporabim še "date" -->
 Leto:
 <?php echo date ('Y'); ?>
 <br>
