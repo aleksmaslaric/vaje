390bd5baae429c7bd5cd57d440a61318bb38146a
jj-am <smucka.smiley@gmail.com>
2020-04-19 18:47:47 +0200

    Dodal še en array in foreach statement.

--- foreach-loop.php
+++ foreach-loop.php
@@ -3,8 +3,14 @@ uporablja za to, da loop-a skozi vrednosti v danem array-u. -->
 
 <?php
   $barve = array('zelena', 'rdeča', 'rumena', 'modra', 'rjava');
-  
+
   foreach ($barve as $vrednost) {
     echo "$vrednost <br>";
+  }
+
+  $starost = array("Aleksandra" => "27", "Anja" => "24", "Miha" => "22", "Željko" => "32");
+
+  foreach ($starost as $x => $vrednost) {
+    echo "$x = $vrednost<br>";
   }
 ?>
