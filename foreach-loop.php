a44a3d3b977b926bc0cfc111920142a10f2ba02a
jj-am <smucka.smiley@gmail.com>
2020-04-19 18:59:16 +0200

    Dodal zelo uporaben komentar na liniji 12

--- foreach-loop.php
+++ foreach-loop.php
@@ -9,7 +9,7 @@ uporablja za to, da loop-a skozi vrednosti v danem array-u. -->
   }
 
   $starost = array("Aleksandra" => "27", "Anja" => "24", "Miha" => "22", "Željko" => "32");
-
+  // $starost = ime array-a, $x = Aleksanra, Anja, Miha in Željko, => $vrednost = 27, 24, 22 in 32, zatorej:
   foreach ($starost as $x => $vrednost) {
     echo "$x = $vrednost<br>";
   }
